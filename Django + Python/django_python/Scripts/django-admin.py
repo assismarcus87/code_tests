43cf7a55142edcf87d43332c55bcbcc2ad2f52a8
Marcus <m.marcusvsa@gmail.com>
2019-09-14 22:28:50 +0000

    Copiando o Projeto do Curso TreinaWeb - Aula2

---
+++ Django + Python/django_python/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\m_mar\Cursos TreinaWeb\Django + Python\django_python\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
